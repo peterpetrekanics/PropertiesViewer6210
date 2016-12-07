213b8bdec130c0952a713ff475fb46b0fc9c5d5f
petrep <petrep@freemail.hu>
2016-12-07 22:12:12 +0100

    initial commit

---
+++ docroot/WEB-INF/src/com/test/PropertiesViewer6210.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PropertiesViewer6210
+ */
+public class PropertiesViewer6210 extends MVCPortlet {
+ 
+
+}
